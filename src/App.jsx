f6814f5ab88083432c995f3cece3d209a19ab819
Sahil-Gulihar <sahilgullihar@gmail.com>
2024-06-21 17:08:59 +0500

    tw

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Game from './components/Game';
 import './App.css';
+import Cards from './components/Cards';
 
 const App = () => {
     return (
         <div className="App">
             <Game />
+            <Cards />
         </div>
     );
 };
