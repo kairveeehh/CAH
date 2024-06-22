e569b10326b0be445d1b3f58d33aee4f58fd6738
kairveeehh <jampu741@gmail.com>
2024-06-22 21:33:16 +0530

    text size

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ const App = () => {
   const [rwc, setrwc] = useState([false, false, false, false, false]);
   const [revealedCardIndex, setRevealedCardIndex] = useState(null);
   const [image,setImage] = useState(true);
-  // State to track which white card is currently revealed
 
   const handleSetSelect = (index) => {
     setSelectedSetIndex(index);
@@ -22,7 +21,7 @@ const App = () => {
     setShowWhiteCards(false);
     setrbc(false);
     setrwc([false, false, false, false, false]);
-    setRevealedCardIndex(null); // Reset revealed card index when a new set is selected
+    setRevealedCardIndex(null);
   };
 
   const drawBlackCard = () => {
@@ -33,7 +32,7 @@ const App = () => {
     setShowWhiteCards(false);
     setrbc(false);
     setrwc([false, false, false, false, false]);
-    setRevealedCardIndex(null); 
+    setRevealedCardIndex(null);
   };
 
   const combinedText = cbc && cwc && cbc.text.includes('_')
@@ -48,7 +47,7 @@ const App = () => {
       newWhiteCards.push(set.white[randomIndex]);
     }
     setHiddenWhiteCards(newWhiteCards);
-    setRevealedCardIndex(null); 
+    setRevealedCardIndex(null);
   };
 
   const revealWhiteCards = () => {
@@ -61,11 +60,9 @@ const App = () => {
 
   const handleWhiteCardClick = (index) => {
     if (revealedCardIndex === index) {
-
       setRevealedCardIndex(null);
       setcwc(null);
     } else {
-
       setRevealedCardIndex(index);
       setcwc(hiddenWhiteCards[index]);
     }
@@ -73,89 +70,96 @@ const App = () => {
 
   return (
     <div className="">
-    <div className="w-full max-w-6xl mx-auto px-4  sm:px-6 lg:px-8 py-12 sm:py-16 lg:py-20">
-      <h1 className="text-3xl font-bold mb-6 text-center">
-        Cards Against Humanity
-      </h1>
-      {selectedSetIndex === null ? (
-        <div className="grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 lg:gap-10">
-          {cardData.map((set, index) => (
-            <div
-              key={index}
-              className="bg-gray-200 rounded-lg shadow-lg overflow-hidden cursor-pointer"
-              onClick={() => handleSetSelect(index)}
-            >
-              <div className="p-4 sm:p-6">
-                <h3 className="text-lg sm:text-xl font-semibold mb-2">{set.name}</h3>
+      <div className="w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12 sm:py-16 lg:py-20">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          Cards Against Humanity
+        </h1>
+        {selectedSetIndex === null ? (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 lg:gap-10">
+            {cardData.map((set, index) => (
+              <div
+                key={index}
+                className="bg-gray-200 rounded-lg shadow-lg overflow-hidden cursor-pointer"
+                onClick={() => handleSetSelect(index)}
+              >
+                <div className="p-4 sm:p-6">
+                  <h3 className="text-lg sm:text-xl font-semibold mb-2">{set.name}</h3>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <div>
-          <div className="flex justify-center"> 
-          <button
-            className="bg-yellow-500  text-white px-4 py-2 rounded mt-4"
-            onClick={drawBlackCard}
-          >
-            Draw Black Card
-          </button>
+            ))}
           </div>
-          <div className="flex justify-center">
-          {cbc && (
-            <div
-              className="bg-black text-white rounded-lg shadow-lg overflow-hidden w-60 h-80 content-center mt-4 cursor-pointer p-4 sm:p-6"
-              onClick={handleBlackCardClick}
-            >
-              <img src={rbc ? cbc.text : "https://i.imgur.com/UxZlfpg.png"} className="w-60" alt="" />
-              <p className="text-gray-300 text-sm sm:text-base">
-                {rbc ? cbc.text : "Click to reveal black card"}
-              </p>
-            </div>
-          )}
-          </div > 
-          {cbc && !showWhiteCards && (
+        ) : (
+          <div>
             <div className="flex justify-center">
-            <button
-              className="bg-yellow-500  text-white px-4 py-2 rounded mt-4"
-              onClick={revealWhiteCards}
-            >
-              Reveal White Cards
-            </button>
+              <button
+                className="bg-yellow-500 text-white px-4 py-2 rounded mt-4"
+                onClick={drawBlackCard}
+              >
+                Draw Black Card
+              </button>
             </div>
-          )}
-          {showWhiteCards && (
-            <div className="grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 lg:gap-10 mt-4">
-              {hiddenWhiteCards.map((card, index) => (
+            <div className="flex justify-center">
+              {cbc && (
                 <div
-                  key={index}
-                  className={`bg-white text-black rounded-lg shadow-lg w-60 h-80 overflow-hidden cursor-pointer p-4 sm:p-6 ${revealedCardIndex === index ? 'border-2 border-gray-200' : ''}`}
-                  onClick={() => handleWhiteCardClick(index)}
+                  className={`bg-black text-white rounded-lg shadow-lg overflow-hidden w-60 h-80 content-center mt-4 cursor-pointer p-4 sm:p-6 ${rbc ? 'text-4xl flex items-center justify-center' : ''}`}
+                  onClick={handleBlackCardClick}
                 >
-                  {revealedCardIndex === index ? <div /> : <img src="https://i.imgur.com/UxZlfpg.png" className="bg-gray-200 h-60" alt="" />}
-                  
-                  <p className="text-gray-500 text-sm flex justify-center align-middle sm:text-base">
-                    {revealedCardIndex === index ? cwc.text : "Click to reveal"}
+                  {rbc ? cbc.text : (
+                    <>
+                      <img src="https://i.imgur.com/UxZlfpg.png" className="w-60" alt="" />
+                      <p className="text-gray-300 text-sm sm:text-base">
+                        Click to reveal black card
+                      </p>
+                    </>
+                  )}
+                </div>
+              )}
+            </div>
+            {cbc && !showWhiteCards && (
+              <div className="flex justify-center">
+                <button
+                  className="bg-yellow-500 text-white px-4 py-2 rounded mt-4"
+                  onClick={revealWhiteCards}
+                >
+                  Reveal White Cards
+                </button>
+              </div>
+            )}
+            {showWhiteCards && (
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 lg:gap-10 mt-4">
+                {hiddenWhiteCards.map((card, index) => (
+                  <div
+                    key={index}
+                    className={`bg-white text-black rounded-lg shadow-lg w-60 h-80 overflow-hidden cursor-pointer p-4 sm:p-6 ${revealedCardIndex === index ? 'border-2 border-gray-200 text-4xl flex items-center justify-center' : ''}`}
+                    onClick={() => handleWhiteCardClick(index)}
+                  >
+                    {revealedCardIndex === index ? cwc.text : (
+                      <>
+                        <img src="https://i.imgur.com/UxZlfpg.png" className="bg-gray-200 h-60" alt="" />
+                        <p className="text-gray-500 text-sm flex justify-center align-middle sm:text-base">
+                          Click to reveal
+                        </p>
+                      </>
+                    )}
+                  </div>
+                ))}
+              </div>
+            )}
+            {cwc && (
+              <div className="bg-gray-800 text-white rounded-lg shadow-lg overflow-hidden mt-4">
+                <div className="p-4 sm:p-6">
+                  <h3 className="text-lg sm:text-xl font-semibold mb-2">
+                    Combined Card
+                  </h3>
+                  <p className="text-gray-300 text-sm sm:text-base">
+                    {combinedText}
                   </p>
                 </div>
-              ))}
-            </div>
-          )}
-          {cwc && (
-            <div className="bg-gray-800 text-white rounded-lg shadow-lg overflow-hidden mt-4">
-              <div className="p-4 sm:p-6">
-                <h3 className="text-lg sm:text-xl font-semibold mb-2">
-                  Combined Card
-                </h3>
-                <p className="text-gray-300 text-sm sm:text-base">
-                  {combinedText}
-                </p>
               </div>
-            </div>
-          )}
-        </div>
-      )}
-    </div>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
